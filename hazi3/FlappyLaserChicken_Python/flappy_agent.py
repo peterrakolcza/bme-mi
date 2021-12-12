90c0d2eacce1798b4f126bd2619ae39bb8fd67e2
Peter Rakolcza <peter.rakolcza@gmail.com>
2021-12-12 21:47:35 +0100

    hazi3 kesz

--- hazi3/FlappyLaserChicken_Python/flappy_agent.py
+++ hazi3/FlappyLaserChicken_Python/flappy_agent.py
@@ -3,7 +3,6 @@ from time import time
 import numpy as np
 np.random.seed(0)
 
-
 class FlappyAgent:
 
     def __init__(self, observation_space_size, action_space, n_iterations):
@@ -14,8 +13,16 @@ class FlappyAgent:
         self.test = False
 
     def step(self, state):
-        # print(state)
-        action = self.max_q_idx(state)
+        temp = self.q_table[state]
+
+        max = -1
+        maxIndex = -1
+        for i in range(len(temp)):
+            if max < temp[i]:
+                max = temp[i]
+                maxIndex = i
+
+        action = maxIndex
 
         if not self.test:  # and ...:
             # action =
@@ -29,23 +36,13 @@ class FlappyAgent:
     def epoch_end(self, epoch_reward_sum):
         pass
 
-    def action_from_q_table(self, old_state, action):
-        return self.q_table[old_state[0]][old_state[1]][old_state[2]][old_state[3]][old_state[4]][action]
-
-    def calc_new_q(self, a, new_state, reward, Q):
-        return a*Q + a*(reward + 0.2*self.max_q_val(new_state))
-
     def learn(self, old_state, action, new_state, reward):
-        a = 0.5
-        Q = self.action_from_q_table(old_state, action)
-        self.q_table[old_state[0]][old_state[1]][old_state[2]][old_state[3]
-                                                               ][old_state[4]][action] = self.calc_new_q(a, new_state, reward, Q)
+        delta = 0.5
+        temp = self.q_table[new_state]
 
-    def max_q_val(self, old_state):
-        return max(self.q_table[old_state[0]][old_state[1]][old_state[2]][old_state[3]][old_state[4]])
+        result = max(temp)
 
-    def max_q_idx(self, old_state):
-        return list(self.q_table[old_state[0]][old_state[1]][old_state[2]][old_state[3]][old_state[4]]).index(max(self.q_table[old_state[0]][old_state[1]][old_state[2]][old_state[3]][old_state[4]]))
+        self.q_table[old_state][action] = delta * self.q_table[old_state][action] + delta * (reward + 0.8 * result)
 
     def train_end(self):
         # ...
