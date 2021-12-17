46ccab873a5ffe8da9c06433758ba188effa536c
Peter Rakolcza <peter.rakolcza@gmail.com>
2021-12-18 00:41:03 +0100

    hazi2 kesz

--- hazi2/hazi2.py
+++ hazi2/hazi2.py
@@ -1,138 +1,143 @@
+import re
 import fileinput
+
+def felsorol_mindent(nodes, evidents):
+    if len(nodes) == 0:
+        return 1.0
+
+    Y = nodes.pop(0)
+    probability = 0
+    if isinstance(evidents[Y.position], type(None)):        
+        for i in range(int(Y.k)):
+            evidents_copy = evidents.copy()
+            evidents_copy[Y.position] = i
+            nodes_copy = nodes.copy()
+            probability += calcProb(Y, evidents_copy) * felsorol_mindent(nodes_copy, evidents_copy)                
+    else:
+        probability = calcProb(Y, evidents.copy()) * felsorol_mindent(nodes.copy(), evidents.copy())
+
+    return probability
+
+
+def calcProb(Y, evidents):
+    parent_values = []
+    for i in range(Y.numOfParents):
+        parent_values.append(evidents[int(Y.parents[i])])
+
+    return Y.probs[tuple(parent_values)][evidents[Y.position]]
+
+def felsorolas_kerdezes(X, e, nodes):
+    Q = []
+    for i in range(int(X.k)):
+        e_copy = e.copy()
+        nodes_copy = nodes.copy()
+        e_copy[X.position] = i
+        Q.append(felsorol_mindent(nodes_copy, e_copy))
+
+    normalizedQ = []
+    for i in range(int(X.k)):
+        normalizedQ.append(Q[i] / sum(Q))        
+
+    return normalizedQ
+
+
+def best_utility(decisionNum, p, u):
+    best_utility = 0
+    best_utility_index = 0
+    for key, e in u.items():
+            if key[1] == 0:
+                best_utility += p[key[0]] * e
+
+    for i in range(decisionNum):
+        utility = 0
+        for key, e in u.items():
+            if key[1] == i:
+                utility += p[key[0]] * e
+        
+        if utility > best_utility:
+            best_utility = utility
+            best_utility_index = i
+
+    return best_utility_index
+
+def init(nodes, utilities, mainNode, decisionNum, evidencies):
+    #comment out for test inputs from file:
+    with fileinput.input() as stdin:
+    #with open(r'input1.txt') as stdin:
+        nodesNum = int(stdin.readline().rstrip())
+
+        for node in range(nodesNum):
+            raw_string = stdin.readline().rstrip()
+            raw_nodes = [float(numeric_string) for numeric_string in re.split("\t|,|:", raw_string)]
+            nodes.append(Node())
+            position = node
+            k = raw_nodes[0]
+            numOfParents = int(raw_nodes[1])
+
+            for p in range(numOfParents):
+                index = p + 2
+                nodes[node].parents.append(raw_nodes[index])
+
+            item = 2 + int(raw_nodes[1])
+            while item < len(raw_nodes):
+                keys = []
+                for j in range(numOfParents):
+                    keys.append(raw_nodes[item + j])
+
+                item += numOfParents
+
+                values = []
+                for j in range(int(k)):
+                    values.append(raw_nodes[item + j])
+
+                item += int(k)
+                nodes[node].probs[tuple(keys)] = values
+
+            nodes[node].position = position
+            nodes[node].k = k
+            nodes[node].numOfParents = numOfParents
+          
+
+        for _ in range(len(nodes)):
+            evidencies.append(None)
+
+        for _ in range(int(stdin.readline().rstrip())):
+            raw_evidents = [int(numeric_string) for numeric_string in stdin.readline().rstrip().split("\t")]
+            evidencies[raw_evidents[0]] = raw_evidents[1]
+
+        mainNode = stdin.readline().rstrip()
+        decisionNum = stdin.readline().rstrip()
+
+        for _ in range(int(decisionNum)*int(nodes[int(mainNode)].k)):
+            raw_decisions = [float(numeric_string) for numeric_string in stdin.readline().rstrip().split("\t")]
+            utilities[tuple([int(raw_decisions[0]), int(raw_decisions[1])])] = raw_decisions[2]
+
+        return nodes, utilities, int(mainNode), int(decisionNum), evidencies
 
 class Node:
     def __init__(self):
-        self.idx = 0
+        self.position = 0
         self.k = 0
-        self.parent_indexes = []
-        self.prob_table = {}
+        self.parents = []
+        self.probs = {}
+        self.numOfParents = 0
 
-
-def normalize(q):
-    Sum = sum(q)
-
-    for i in range(len(q)):
-        q[i] = q[i] / Sum
-
-    return q
-
-
-def felsorol_mindent(bn, e):
-    if not bn:
-        return 1.0
-
-    Y = bn.pop(0)
-    y_value = e[Y.idx]
-
-    if y_value != None:
-        return getProbability(Y, y_value, e) * felsorol_mindent(bn.copy(), e.copy())
-    else:
-        prob_sum = 0
-        for i in range(Y.k):
-            temp = e.copy()
-            temp[Y.idx] = i
-
-            prob_sum += getProbability(Y, i, e) * felsorol_mindent(bn.copy(), temp)
-
-        return prob_sum
-
-
-def getProbability(Y, y_value, e):
-    parent_values = []
-    for i in range(len(Y.parent_indexes)):
-        parent_values.append(e[Y.parent_indexes[i]])
-
-    if not Y.parent_indexes:
-        return Y.prob_table[tuple([])][y_value]
-
-    return Y.prob_table[tuple(parent_values)][y_value]
-
-def felsorolas_kerdezes(X, e, bn):
-    Q = [None] * X.k
-
-    temp = e.copy()
-    for x_value in range(X.k):
-        temp[X.idx] = x_value
-        Q[x_value] = felsorol_mindent(bn.copy(), temp)
-
-    return normalize(Q)
-
-
-def expected_utilities(Q, expediencies, d):
-    calculated_utilities = []
-    for i in range(d):
-        expected_utility = 0
-
-        for key, e in expediencies.items():
-            if key[1] == i:
-                expected_utility += Q[key[0]] * e
-        calculated_utilities.append(expected_utility)
-
-    return calculated_utilities
-
-#comment out for test inputs from file:
-#with open(r'input2.txt') as f:
-with fileinput.input() as f:
-    numOfNodes = int(f.readline().rstrip())
-
-    nodes = [Node() for i in range(numOfNodes)]
-
-    for i in range(numOfNodes):
-        read_items = f.readline().rstrip().split("\t")
-
-        nodes[i].idx = i
-        nodes[i].k = int(read_items[0])
-        num_indexes = int(read_items[1])
-        parent_indexes = []
-
-        # read parent index values
-        for j in range(num_indexes):
-            parent_indexes.append(int(read_items[j + 2]))
-
-        nodes[i].parent_indexes = parent_indexes
-
-        # read parents values
-        for j in range(len(read_items) - num_indexes - 2):
-            temp = read_items[num_indexes + 2 + j].split(":")
-            keys = []
-            values = []
-            if len(temp) == 1:
-                values = temp[0].split(",")
-            else:
-                keys = temp[0].split(",")
-                values = temp[1].split(",")
-
-            values = list(map(float, values))
-            keys = list(map(int, keys))
-            
-            nodes[i].prob_table[tuple(keys)] = values
-            
-
-    numOfEvidencyVariables = int(f.readline().rstrip())
-    evidency_variables = [None] * numOfNodes
-
-    # read evidency variables
-    for i in range(numOfEvidencyVariables):
-        read_items = f.readline().rstrip().split("\t")
-        evidency_variables[int(read_items[0])] = int(read_items[1])
-
-    indexOfGoalNode = int(f.readline().rstrip())
-
-    numOfDecisions = int(f.readline().rstrip())
-
+def main():
+    nodes = []
     utilities = {}
+    mainNode = 0
+    decisionNum = 0
+    evidencies = []
 
-    for i in range(nodes[indexOfGoalNode].k * numOfDecisions):
-        read_items = f.readline().rstrip().split("\t")
-        utilities[tuple([int(read_items[0]), int(read_items[1])])] = float(read_items[2])
+    nodes, utilities, mainNode, decisionNum, evidencies = init(nodes, utilities, mainNode, decisionNum, evidencies)
 
     #calculate
-    Q = felsorolas_kerdezes(nodes[indexOfGoalNode], evidency_variables, nodes)
+    Q = felsorolas_kerdezes(nodes[mainNode], evidencies, nodes)
 
     for p in Q:
         print(p)
+    
+    print(best_utility(decisionNum, Q, utilities))
 
-    #calculate best utility
-    expected_decision = expected_utilities(Q, utilities, numOfDecisions)
-
-    print(expected_decision.index(max(expected_decision)))
+if __name__ == "__main__":
+    main()
